b91017219f084b4097846205e14fa627e77b8bfa
Juju Nakarmi <152660757+Jujunakarmi@users.noreply.github.com>
2024-08-14 17:39:35 -0400

    added tables in admin panel

--- client/src/pages/AllUsers.jsx
+++ client/src/pages/AllUsers.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import moment from 'moment'
 import { getAllUsers } from '../utils/API';
+import { toast } from 'react-hot-toast';
+import { MdEdit } from "react-icons/md";
 
 const AllUsers = () => {
   const [allUsers, setAllUsers] = useState([]);
@@ -33,22 +36,29 @@ const AllUsers = () => {
           <th>Email</th>
           <th>Role</th>
           <th>Created Date</th>
+          <th>Action</th>
           </tr>
         </thead>
         <tbody>
-       {
+        {
         allUsers.map((el, index) =>  {
           return (
-            <tr>
+            <tr key={el._id}>
               <td>{index+1}</td>
               <td>{el?.name}</td>
               <td>{el?.email}</td>
               <td>{el?.role}</td>
-              <td>{el?.createdAt}</td>
+              <td>{moment(el?.createdAt).format('ll')}</td>
+              <td >
+                <button>
+                <MdEdit className='text-2xl bg-green-200  p-1 rounded-full hover:bg-green-600 hover:text-white'  />
+
+                </button>
+                </td>
             </tr>
           )
         })
-       }
+       } 
         </tbody>
       </table>
     </div>
