47f1b15ef611229612637d0486c8808bdde81f30
Juju Nakarmi <152660757+Jujunakarmi@users.noreply.github.com>
2024-08-23 18:14:09 -0400

    added admin edit product page for editing products

--- client/src/components/AdminProductCard.jsx
+++ client/src/components/AdminProductCard.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { MdModeEdit } from "react-icons/md";
+
 
 const AdminProductCard = ({
-data
+  data
 }) => {
   return (
-    <div>
-          <div className='bg-white p-4 rounded'>
-        <img src={data?.productImage[0]} height={100} width={100}/>
-        <h1>{data.productName}</h1>
-        </div>
+
+    <div className='bg-white p-4 rounded'>
+      <img src={data?.productImage[0]} height={100} width={100} />
+      <h1>{data.productName}</h1>
+
+      <div className='w-fit ml-auto p-1 bg-green-200  hover:bg-green-600 rounded-full hover:text-white cursor-pointer'>
+        <MdModeEdit />
+      </div>
+
+
     </div>
+
+
   )
 }
 
